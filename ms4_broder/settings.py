f89645a921c811982cde3d416ae78d54656859ba
Federvgh <caffarofederico@gmail.com>
2021-10-21 16:27:41 +0000

    added checkout view and templates

--- ms4_broder/settings.py
+++ ms4_broder/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
-    
+        # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ms4_broder.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -79,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
