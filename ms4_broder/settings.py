cd32be2f5f46a23d140099431a9e10415bd5c910
Federvgh <caffarofederico@gmail.com>
2021-10-25 18:54:58 +0000

    deployment part 2

--- ms4_broder/settings.py
+++ ms4_broder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6zt(euizxg+-2rc8laa@)7hvbl0(bj3y1556ej3u((x(t7w7))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-broder.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,13 +121,17 @@ WSGI_APPLICATION = 'ms4_broder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
