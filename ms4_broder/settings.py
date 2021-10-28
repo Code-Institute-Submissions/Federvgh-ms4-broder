51904c7e4641a7fce67e4eb23482fa3d54f936ac
Federvgh <caffarofederico@gmail.com>
2021-10-28 00:37:33 +0000

    media files uploaded again

--- ms4_broder/settings.py
+++ ms4_broder/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['ms4-broder.herokuapp.com', 'localhost']
 
